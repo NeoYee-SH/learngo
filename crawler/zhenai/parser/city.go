a68b6288727f6d6a3095f780f34c033583169f3b
yihuaiyuan <yihuaiyuan@qutoutiao.net>
2020-01-14 21:06:24 +0800

    获取更多城市的数据

--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -6,11 +6,13 @@ import (
 	"github.com/yihuaiyuan/learngo/crawler/engine"
 )
 
-const cityRe = `<a href="(http://album.zhenai.com/u/[\d]+)"[^>]+>([^<]+)</a></th></tr> <tr><td width="180"><span class="grayL">性别：</span>([^<]+)</td> `
+var (
+	profileRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[\d]+)"[^>]+>([^<]+)</a></th></tr> <tr><td width="180"><span class="grayL">性别：</span>([^<]+)</td> `)
+	cityUrlRe = regexp.MustCompile(`href="(http://www.zhenai.com/zhenghun/[^"]+)"`)
+)
 
 func ParseCity(contents []byte) engine.ParseResult {
-	re := regexp.MustCompile(cityRe)
-	matches := re.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
@@ -24,5 +26,14 @@ func ParseCity(contents []byte) engine.ParseResult {
 		})
 		result.Items = append(result.Items, name)
 	}
+
+	matches = cityUrlRe.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		result.Requests = append(result.Requests, engine.Request{
+			Url:       string(m[1]),
+			ParseFunc: ParseCity,
+		})
+	}
+
 	return result
 }
